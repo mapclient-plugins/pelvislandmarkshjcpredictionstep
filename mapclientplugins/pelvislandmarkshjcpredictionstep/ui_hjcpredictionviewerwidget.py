67584462e348a59e61b9a7e46733123220fe50d2
juzhang <jzha263@aucklanduni.ac.nz>
2014-11-03 16:49:29 +1300

    fixed mappluginutil import statement

--- mapclientplugins/pelvislandmarkshjcpredictionstep/ui_hjcpredictionviewerwidget.py
+++ mapclientplugins/pelvislandmarkshjcpredictionstep/ui_hjcpredictionviewerwidget.py
@@ -218,4 +218,4 @@ class Ui_Dialog(object):
         self.screenshotFilenameLineEdit.setText(QtGui.QApplication.translate("Dialog", "screenshot.png", None, QtGui.QApplication.UnicodeUTF8))
         self.screenshotSaveButton.setText(QtGui.QApplication.translate("Dialog", "Save Screenshot", None, QtGui.QApplication.UnicodeUTF8))
 
-from mayaviscenewidget import MayaviSceneWidget
+from mappluginutils.mayaviviewer.mayaviscenewidget import MayaviSceneWidget
